d4da92f5ee8af232e3367d650df7d25992609b1a
kishan-dobariya <kishan.dobariya@bacancytechnology.com>
2018-05-22 13:53:19 +0530

    first

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const app = express()
+app.get('/', (req, res) => res.send('Hello World!'))
